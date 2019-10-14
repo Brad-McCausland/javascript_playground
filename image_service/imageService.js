c7f4535816c2258d235a89f6519daa8e608962af
bmcc0605 <mccausland0605@gmail.com>
2019-10-14 15:29:57 -0700

    Playing with html and css. Modified image service to send album/file names in json.

--- image_service/imageService.js
+++ image_service/imageService.js
@@ -3,15 +3,24 @@ const express = require('express');
 const cors = require('cors');
 
 const imagePortNum = 8001;
-const imageDirectory = fs.readdirSync("images/");
+const allImageDirectories = ["album_images", "other_images"]
 
-// Load each file in images directory, convert them to Base64, and push the base64 data onto images[]
-var images = [];
-imageDirectory.forEach (function(fileName)
+var images = {};
+var imageCount = 0;
+
+// Load all files in all image directories. Keyed by directory name then file name.
+allImageDirectories.forEach (function (dirName)
 {
-  let file = "images/" + fileName;
-  encodedImage = Buffer(fs.readFileSync(file)).toString('base64');
-  images.push(encodedImage);
+  let currentDirectoryImages = {};
+  const currentDirectory = fs.readdirSync(dirName);
+  currentDirectory.forEach (function(fileName)
+  {
+    let file = dirName + "/" + fileName;
+    encodedImage = Buffer(fs.readFileSync(file)).toString('base64');
+    currentDirectoryImages[fileName] = encodedImage;
+    imageCount++;
+  });
+  images[dirName] = currentDirectoryImages;
 });
 
 // Convert array to JSON
@@ -23,7 +32,7 @@ app.use(express.static(__dirname));
 app.use(cors());
 app.get('/', function(req, res)
 {
-  console.log("Serving " + images.count + " images");
+  console.log("Serving " + imageCount + " images");
   res.send(imageData);
 });
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,36 @@
-// Raw json data from image service
-const imageData = httpGet("http://localhost:8001/");
-
-// Array of images in Base64 
-const imageArray = JSON.parse(imageData);
 
 // Array to be populated with finished Image objects
-var imageFiles = [];
+var slideshowImages = [];
 
-// Convert each element in imageArray from Base64 into an image object. Push finished objects onto imageFiles[].
-imageArray.forEach(function(imageData) {
-    let src = "data:image/jpeg;base64,";;
-    src += imageData;
-    let image = new Image();
-    image.src = src;
-    imageFiles.unshift(image);
-});
+// Try loading images from image service
+try
+{
+    // Raw json data from image service
+    const imageData = httpGet("http://localhost:8001/");
+
+    // Array of images in Base64 
+    const allImages = JSON.parse(imageData);
+
+    // Convert each file under 'album_images' from Base64 into an image object. Push finished objects onto slideshowImages[].
+    var albumImages = allImages["album_images"]
+    Object.keys(albumImages).forEach(function(key)
+    {
+        let src = "data:image/jpeg;base64,"
+        src += albumImages[key];
+        let image = new Image();
+        image.src = src;
+        slideshowImages.unshift(image);
+    });
+}
+catch (error)
+{
+    // If image service cannot be reached, push error message into slideshowImages[] instead
+    let errorImage = new Image();
+    errorImage.src = "image_service/other_images/image_load_error.png";
+    
+    slideshowImages.unshift(errorImage);
+    console.log("catch activated: " + error);
+}
 
 // Tracks index of currently-displayed image
 var currentImageIndex = 0;
@@ -27,6 +43,7 @@ heading.textContent = "Glory to you!";
 var canvas = document.createElement('canvas');
 canvas.width = 400;
 canvas.height = 400;
+canvas.display = "inline-block"
 document.body.appendChild(canvas);
 
 canvas.onclick = function()
@@ -42,16 +59,16 @@ window.onload = function()
 function drawNextImage()
 {
     var context = canvas.getContext('2d');
-    currentImageIndex = (currentImageIndex + 1) % imageFiles.length
-    let newImage = imageFiles[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % slideshowImages.length
+    let newImage = slideshowImages[currentImageIndex];
     context.drawImage(newImage, 0, 0, 400, 400);
 }
 
-function httpGet(theUrl)
+function httpGet(url)
 {
     var xmlHttp = new XMLHttpRequest();
     //TODO: Remove syncronous call on the main thread
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
